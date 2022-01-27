af0cc67fecb5744f2ef0dab4c57a0f757892f1d0
EddieFreedome <eddie.freed@tutanota.com>
2022-01-27 17:17:59 +0100

    Created migration and imported db

---
+++ database/migrations/2022_01_26_164557_create_travel_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travel', function (Blueprint $table) {
+            $table->id();
+            $table->string('departure');
+            $table->string('destination');
+            $table->smallInteger('departureTime');
+            $table->smallInteger('arrivalTime');//serve un tipo decimale
+            $table->integer('Price');
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travel');
+    }
+}
